a148d8fa1b526628a60d09530c9772a43b6320af
rampop01 <popoolarahmat96@gmail.com>
2026-05-16 18:54:45 +0100

    feat(sdk): add validation constants to config.ts

--- sdk/config.ts
+++ sdk/config.ts
@@ -7,5 +7,12 @@ export const SDK_CONFIG = {
   API_URL: 'https://api.mainnet.hiro.so',
 };
 
+export const VALIDATION = {
+  MIN_TITLE_LENGTH: 5,
+  MAX_TITLE_LENGTH: 100,
+  MIN_OPTIONS: 2,
+  MAX_OPTIONS: 2, // Currently only supporting binary duels
+};
+
 export const getContractIdentifier = () => 
   `${SDK_CONFIG.CONTRACT_ADDRESS}.${SDK_CONFIG.CONTRACT_NAME}`;
