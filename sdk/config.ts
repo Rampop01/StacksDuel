3e8744ccbe7e4bd5946433d8b7bf75b259785d6f
rampop01 <popoolarahmat96@gmail.com>
2026-05-16 18:33:13 +0100

    fix(sdk): add NETWORK to SDK_CONFIG

--- sdk/config.ts
+++ sdk/config.ts
@@ -1,4 +1,7 @@
+import { STACKS_MAINNET } from '@stacks/network';
+
 export const SDK_CONFIG = {
+  NETWORK: STACKS_MAINNET,
   CONTRACT_ADDRESS: 'SP1BTBG1TW13NEV2FQM7HC1BZ9XZV7FZSGPMVV38M',
   CONTRACT_NAME: 'duel-engine',
   API_URL: 'https://api.mainnet.hiro.so',
