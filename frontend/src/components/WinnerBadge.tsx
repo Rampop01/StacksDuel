bca9e75ff4630c9e5fcfcbbee9ac7c89703379c7
rampop01 <popoolarahmat96@gmail.com>
2026-05-22 00:03:36 +0100

    feat: Add visual indicator for Resolved duels

---
+++ frontend/src/components/WinnerBadge.tsx
@@ -0,0 +1,4 @@
+import { Trophy } from './Icons';
+export default function WinnerBadge() {
+    return <div className="absolute top-2 right-2 p-2 bg-yellow-500 rounded-full shadow-lg"><Trophy className="text-black w-6 h-6" /></div>;
+}
