a5a80120a0e9aef97b56adb054ff4ebfe1aa3190
rampop01 <popoolarahmat96@gmail.com>
2026-05-22 00:03:36 +0100

    feat: Build Admin Oracle interface to resolve duels

---
+++ frontend/src/components/AdminPanel.tsx
@@ -0,0 +1,4 @@
+'use client';
+export default function AdminPanel({ onResolve }: { onResolve: (id: string, winner: number) => void }) {
+    return <div className="p-6 border border-rose-500/50 rounded-xl bg-black"><h2>Admin Resolution</h2></div>;
+}
